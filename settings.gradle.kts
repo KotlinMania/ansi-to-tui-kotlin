b2bca2ede0f4376de8309688d02b983b13530aea
Sydney Renee <sydney@solace.ofharmony.ai>
2025-12-22 22:50:43 -0800

    Updating targets and version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,17 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
+        google()
         mavenCentral()
+        gradlePluginPortal()
     }
+    plugins { kotlin("jvm") version "2.3.0" }
 }
 
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0" }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
         mavenCentral()
     }
 }
